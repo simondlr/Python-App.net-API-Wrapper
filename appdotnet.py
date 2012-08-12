9480463d6056c23f4781c73394e918a266bf5f46
Simon de la Rouviere <simon@delarouviere.com>
2012-08-12 20:03:38 +0200

    Added most get functions. Moved from urllib2 to python-requests. Changed up constructor.

--- appdotnet.py
+++ appdotnet.py
@@ -1,21 +1,39 @@
-import urllib2
-import urllib
+import requests
+import json
+
+#endpoints still to add.
+# - POST/DELETE endpoints
+# - Retrieve replies to post (in development by app.net)
+# - General parameters (in dev by app.net)
+# - Streams (in dev by app.net)
+# - Filters (in dev by app.net)
 
 class appdotnet:
-	def __init__(self,client_id,client_secret,redirect_uri,scope):
+	'''
+	Once access has been given, you don't have to pass through the client_id, client_secte, redirect_uri, or scope. These are just to get the authentication token.
+
+	Once authenticated, you can initialise appdotnet with only the access token: ie
+
+	api = appdotnet(access_token='<insert token here>')
+	'''
+
+	def __init__(self,client_id=None,client_secret=None,redirect_uri=None,scope=None,access_token=None):
 		#for server authentication flow
-		#check for invalid formats.
 		self.client_id = client_id
 		self.client_secret = client_secret
 		self.redirect_uri = redirect_uri
 		self.scope = scope
+
+		self.access_token = access_token
+
 		self.api_anchor = "alpha.app.net" #for when the versions change
+		#anchors currently different
+		self.public_api_anchor = "alpha-api.app.net" #for when the versions change
 
 		#scopes provided by app.net API
 		self.allowed_scopes = ['stream','email','write_post','follow','messages','export']
 
 	def generateAuthUrl(self):
-		#question: if scope is empty, but not just ommitted, does it ask for permission to nothing?
 		url = "https://"+self.api_anchor+"/oauth/authenticate?client_id="+self.client_id+"&response_type=code&redirect_uri="+self.redirect_uri+"&scope="
 
 		for scope in self.scope:
@@ -33,10 +51,49 @@ class appdotnet:
 		'redirect_uri':self.redirect_uri,
 		'code':code}
 
-		data = urllib.urlencode(post_data)
-
-		req = urllib2.Request(url,data)
-		response = urllib2.urlopen(req)
+		r = requests.post(url,data=post_data)
 		
-		return response.read()
+		return r.text
 	
+	'''
+	API Calls
+	'''
+
+	def getCall(self,url):
+		#access token
+		url = url+"?access_token="+self.access_token
+		r = requests.get(url)
+		if r.status_code == requests.codes.ok:
+			return r.text
+		else:
+			j = json.loads(r.text)
+			return "{'error_code':"+(str)(r.status_code)+",'message':'"+j['error']['message']+"'}"
+		
+
+	def getUser(self,user_id):
+		url = "https://"+self.public_api_anchor+"/stream/0/users/"+user_id
+		return self.getCall(url)
+
+	def getUserPosts(self,user_id):
+		url = "https://"+self.public_api_anchor+"/stream/0/users/"+user_id+"/posts"
+		return self.getCall(url)
+	
+	def getGlobalStream(self):
+		url = "https://"+self.public_api_anchor+"/stream/0/posts/stream/global"
+		return self.getCall(url)
+
+	def getUserStream(self):
+		url = "https://"+self.public_api_anchor+"/stream/0/posts/stream"
+		return self.getCall(url)
+
+	def getUserMentions(self,user_id):
+		url = "https://"+self.public_api_anchor+"/stream/0/users/"+user_id+"/mentions"
+		return self.getCall(url)
+
+	def getPost(self,post_id):
+		url = "https://"+self.public_api_anchor+"/stream/0/posts/"+post_id
+		return self.getCall(url)
+
+
+
+
